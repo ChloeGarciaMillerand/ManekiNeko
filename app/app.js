282e2645d7007f8fa3539bc1114f2eaa7930e3f6
Chloé Garcia <193918132+ChloeGarciaMillerand@users.noreply.github.com>
2025-02-15 14:56:30 +0100

    add comparison between 2 values

--- app/app.js
+++ app/app.js
@@ -22,11 +22,25 @@ for (let object of objects) {
 };
 */
 
-// récupérer la valeur de l'input réponse
+// variable qui indique à quel index du tableau objet chercher la valeur:
+let indexObject = 0;
+
+// stocker la valeur de l'input dans numberUser
+let numberUser;
 
 const form = document.getElementById('game-form');
+
+// ajouter un écouteur d'événément à la soumission du formulaire
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    const input = event.target.elements["object-price"];
-    console.log(input.value);
+    let input = event.target.elements["object-price"];
+    numberUser = parseInt(input.value);
+    console.log(numberUser);
+
+    // comparer la valeur de l'input à celle de l'objet
+    if (numberUser === objects[indexObject].price) {
+        console.log("trouvé!");
+    } else {
+        console.log("perdu!");
+    }
 });
